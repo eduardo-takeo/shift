bc489bf403e6a1f3a8ccc8376ba1d5c1f7c540c1
eduardo-takeo <dudu.takeo96@gmail.com>
2020-03-04 11:11:59 -0300

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return(
+        <header>
+            <div className="date">{getDate()}</div>
+            <div className="logo">Shift</div>
+            <div className="categories">
+                <ul>
+                    <a href="#" className="categories-item">General</a>
+                    <a href="#" className="categories-item">Business</a>
+                    <a href="#" className="categories-item">Entertainment</a>
+                    <a href="#" className="categories-item">Health</a>
+                    <a href="#" className="categories-item">Science</a>
+                    <a href="#" className="categories-item">Technology</a>
+                    <a href="#" className="categories-item">Sports</a>
+                </ul>
+            </div>
+        </header>
+    )
+}
+
+function getDate() {
+    const dateObj = new Date();  
+    const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };  
+
+    return (dateObj.toLocaleDateString("en-US", options)); 
+}
+
+export default Header;
